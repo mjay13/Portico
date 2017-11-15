bb485100be5d47a37b1b22ae458a80bf31718492
Megan Jacobs <mjay815@hotmail.com>
2017-11-15 14:13:44 -0500

    first item get all route

--- controllers/artist_controller.js
+++ controllers/artist_controller.js
@@ -1,3 +1,59 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
+
+
+module.exports = function(router) {
+
+        // get all artists in ascending order
+        router.get("/artist/ref-num/:id", function(req, res) {
+                
+                if (req.params.id) {
+
+                    db.artist.findOne({
+                        where: {
+                            artist_reference_number: req.params.id
+                        }
+                    }).then(function(ref_num) {
+                        //return res.json(result);
+                        var hbsObject = {
+                            artist: ref_num
+                        };
+                        return res.render("artist", hbsObject);
+                    });
+                }
+
+                // Otherwise...
+                else {
+                	// error message instead?
+                    artist.findAll({
+
+                    })
+                        .then(function(result) {
+                            return res.json(result);
+                        });
+                }
+            },
+
+
+            // get all artists in ascending order
+            router.get("/artist", function(req, res) {
+                // replace old function with sequelize function
+                db.artist.findAll({
+                        // Here we specify we want to return our burgers in ordered by ascending burger_name
+                        order: [
+                            ["artist_reference_number", "ASC"]
+                        ]
+                    })
+                    // use promise method to pass the burgers...
+                    .then(function(dbArtist) {
+                        // into the main index, updating the page
+                        var hbsObject = {
+                            artist: dbArtist
+                        };
+                        return res.render("artist", hbsObject);
+                    });
+            })
+
+            // end of export
+        )};
--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -2,33 +2,54 @@ var express = require("express");
 var router = express.Router();
 var db = require("../models");
 
-// var express = require("express");
-// var router = express.Router();
-var db = require("../models");
 
 module.exports = function(router) {
-// get route -> index
-router.get("/", function(req, res) {
-  // send us to the next get function instead.
-  res.redirect("/index");
-});
+    // get route -> index
+    router.get("/", function(req, res) {
+        // send us to the next get function instead.
+        res.render("index");
+    });
 
-// get route, edited to match sequelize
-router.get("/catalog", function(req, res) {
-  // replace old function with sequelize function
-  db.item.findAll({
-    // Here we specify we want to return our burgers in ordered by ascending burger_name
-    order: [
-      ["reference_number", "ASC"]
-    ]
-  })
-  // use promise method to pass the burgers...
-  .then(function(dbItem) {
-    // into the main index, updating the page
-    var hbsObject = {
-      item: dbItem
-    };
-    return res.render("catalog", hbsObject);
-  });
-});
+    // get for all items in ascending order
+    router.get("/catalog", function(req, res) {
+
+        db.item.findAll({
+
+                order: [
+                    ["item_reference_number", "ASC"]
+                ]
+            })
+
+            .then(function(dbItem) {
+
+                var hbsObject = {
+                    item: dbItem
+                };
+                return res.render("catalog", hbsObject);
+            });
+    });
+    // get all items with the same artist
+    router.get("/catalog/artistitems/:artistref", function(req, res) {
+
+        db.item.findAll({
+                // with a foriegn key for the artist
+                // make into a variable
+                // artistId
+
+            })
+            .then(function(dbItem) {
+                // pass the variable, and look for the artist_reference_number that equals the req.params.artist-ref
+                where: {
+                    artist_reference_number: req.params.artistref
+                }
+
+
+                var hbsObject = {
+                    item: dbItem
+                };
+                return res.render("catalog", hbsObject);
+            });
+    });
+
+    // end of export
 };
