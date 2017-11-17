faf17a8ee70e2ddeabff3e1623bcd778983f6251
Megan Jacobs <mjay815@hotmail.com>
2017-11-16 19:29:56 -0500

    search for artist on artist page started, handlebars needs tweaking

--- public/assets/js/searches.js
+++ public/assets/js/searches.js
@@ -3,29 +3,36 @@ $(document).ready(function() {
     // on the click of go button, take search params, and put them in the route /catalog/item/byartist/:params 
     // then call that route
     // the page will redirect and load within the catalouge section
+
+    // catalog page all items by artist
     $(document).on("click", "#text-enter-button", itemsByArtist);
+    // catalog page all items show
     $(document).on("click", "#show-all-items", allItems);
+    // artist page one artist show
     $(document).on("click", "#ref-enter-button", byArtistRef);
 
 
 
-
-        // id = $("#search-bar");
-
-        // $.ajax({
-        //     method: "GET",
-        //     url: "/catalog/item/byartist/" + id
-        // });
-
-    }
-
     function allItems() {
-       $.get("/catalog/all-items");
-       console.log("btn clicked");
-       // pulled this from in-class activity 11.04.2017 15.3 cms.js
-       window.location.href = "/catalog/all-items"; 
+        $.get("/catalog/all-items");
+        console.log("btn clicked");
+        // pulled this from in-class activity 11.04.2017 15.3 cms.js
+        window.location.href = "/catalog/all-items";
     }
 
+
+    function itemsByArtist(event) {
+        event.preventDefault();
+        console.log("btn clicked");
+        id = $("#num-input").val().trim();
+        console.log(id);
+
+        $.ajax({
+            method: "GET",
+            url: "/catalog/item/by-artist/" + id
+        }).done(window.location.href = "/catalog/item/by-artist/" + id);
+
+    }
 
     function byArtistRef(event) {
         event.preventDefault();
@@ -51,6 +58,5 @@ $(document).ready(function() {
 
 
 
-
-// end of document.ready
+    // end of document.ready
 });
